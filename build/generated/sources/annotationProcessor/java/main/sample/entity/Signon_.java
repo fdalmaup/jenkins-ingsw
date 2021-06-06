2ac355aef21414c0eada3d96ee4955f00d428007
Facundo Dalmau <dalmaufacundo@gmail.com>
2021-06-06 14:19:34 -0700

    Copia spring-boot-jpetstore

---
+++ build/generated/sources/annotationProcessor/java/main/sample/entity/Signon_.java
@@ -0,0 +1,43 @@
+package sample.entity;
+
+/** */
+@javax.annotation.Generated(value = { "Doma", "2.46.2" }, date = "2021-06-06T13:42:33.361-0700")
+public final class Signon_ implements org.seasar.doma.jdbc.criteria.metamodel.EntityMetamodel<sample.entity.Signon> {
+
+    static {
+        org.seasar.doma.internal.Artifact.validateVersion("2.46.2");
+    }
+
+    private final String __qualifiedTableName;
+
+    private final sample.entity._Signon __entityType = sample.entity._Signon.getSingletonInternal();
+
+    private final java.util.List<org.seasar.doma.jdbc.criteria.metamodel.PropertyMetamodel<?>> __allPropertyMetamodels;
+
+    public final org.seasar.doma.jdbc.criteria.metamodel.PropertyMetamodel<java.lang.String> username = new org.seasar.doma.jdbc.criteria.metamodel.DefaultPropertyMetamodel<java.lang.String>(java.lang.String.class, __entityType, "username");
+
+    public final org.seasar.doma.jdbc.criteria.metamodel.PropertyMetamodel<java.lang.String> password = new org.seasar.doma.jdbc.criteria.metamodel.DefaultPropertyMetamodel<java.lang.String>(java.lang.String.class, __entityType, "password");
+
+    public Signon_() {
+        this("");
+    }
+
+    public Signon_(String qualifiedTableName) {
+        this.__qualifiedTableName = java.util.Objects.requireNonNull(qualifiedTableName);
+        java.util.ArrayList<org.seasar.doma.jdbc.criteria.metamodel.PropertyMetamodel<?>> __list = new java.util.ArrayList<>(2);
+        __list.add(username);
+        __list.add(password);
+        __allPropertyMetamodels = java.util.Collections.unmodifiableList(__list);
+    }
+
+    @Override
+    public org.seasar.doma.jdbc.entity.EntityType<sample.entity.Signon> asType() {
+        return __qualifiedTableName.isEmpty() ? __entityType : new org.seasar.doma.jdbc.criteria.metamodel.EntityTypeProxy<>(__entityType, __qualifiedTableName);
+    }
+
+    @Override
+    public java.util.List<org.seasar.doma.jdbc.criteria.metamodel.PropertyMetamodel<?>> allPropertyMetamodels() {
+        return __allPropertyMetamodels;
+    }
+
+}
